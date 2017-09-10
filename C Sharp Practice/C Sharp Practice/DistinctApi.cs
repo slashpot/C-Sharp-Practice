db47dd876bd680235835323be653fd7c2a13044b
Stan Lin <ttttt123465@gmail.com>
2017-09-10 17:22:37 +0800

    add distice collection practice

---
+++ C Sharp Practice/C Sharp Practice/DistinctApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DistinctCollection
+{
+    public class DistinctApi
+    {
+        public object GetDistinct(IEnumerable<int> source)
+        {
+            HashSet<int> hashset = new HashSet<int>();
+            GetDistinctHashSetElements(hashset, source);
+            int[] distincts = hashset.ToArray<int>();
+
+            var sourceType = source.GetType();
+            var result = Activator.CreateInstance(sourceType, distincts);
+            return result;
+        }
+
+        private void GetDistinctHashSetElements(HashSet<int> hashset, IEnumerable<int> source)
+        {
+            var enumerator = source.GetEnumerator();
+            while (enumerator.MoveNext())
+                if (!hashset.Contains(enumerator.Current))
+                    hashset.Add(enumerator.Current);
+        }
+    }
+}
---
+++ C Sharp Practice/UnitTestProject/DistinctApi_test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using DistinctCollection;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class DistinctApi_test
+    {
+        [Test]
+        public void Input_Two_Same_Nums_And_One_Unique_Num_Should_Retun_Two_Unique_Nums()
+        {
+            var target = new DistinctApi();
+            var input = new List<int> { 1, 1, 2 };
+
+            var actual = target.GetDistinct(input);
+            var expected = new List<int> { 1, 2 };
+
+            Assert.AreEqual(true, Equals((List<int>)actual, expected));
+        }
+
+        private bool Equals(List<int> a, List<int> b)
+        {
+            if (a == null) return b == null;
+            if (b == null || a.Count != b.Count) return false;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (!Equals(a[i], b[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+}
